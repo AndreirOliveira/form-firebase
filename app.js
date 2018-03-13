7525b0a262ec156fc562a4fb2c1d9579c45686ac
ANDREIR PIRES DE OLIVEIRA <andreir.poliveira@senacsp.edu.br>
2018-03-12 21:56:42 -0300

    versao 120318

---
+++ app.js
@@ -0,0 +1,10 @@
+ // Initialize Firebase
+ var config = {
+  apiKey: "AIzaSyCrJczJcwMiBG-zTTaYbHUbvIH9cdba7DE",
+  authDomain: "bikcraft-11ff6.firebaseapp.com",
+  databaseURL: "https://bikcraft-11ff6.firebaseio.com",
+  projectId: "bikcraft-11ff6",
+  storageBucket: "bikcraft-11ff6.appspot.com",
+  messagingSenderId: "598203752168"
+};
+firebase.initializeApp(config);
